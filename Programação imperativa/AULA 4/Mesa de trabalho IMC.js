acedaf3f7355500a5a0550b3bb95de19d851732c
Jonata santos <89052236+Jonatacosta@users.noreply.github.com>
2021-11-03 22:15:11 -0300

    Add files via upload

---
+++ Programação imperativa/AULA 4/Mesa de trabalho IMC.js
@@ -0,0 +1,8 @@
+var nome = "josé";
+var idade = 27;
+var peso = 83.5;
+var altura = 3.40;
+var plano = "Diamante";
+var IMC = (peso/altura);
+
+console.log(nome +  'tem'  +  idade  +  'anos de idade e seu IMC é de' + IMC);
